43c7e5c99ecf22056a1ba7c2dcfeab3cac5f179e
Oswald Graae <78157610+Selfsson-Dev@users.noreply.github.com>
2024-12-06 16:45:57 +0100

    Hello World!

---
+++ SystemClass.h
@@ -0,0 +1,40 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+/*
+#include "InputClass.h"
+#include "ApplicationClass.h"
+*/
+
+class SystemClass
+{
+public:
+	SystemClass();
+	SystemClass(const SystemClass&);
+	~SystemClass();
+
+	bool initialize();
+	void shutdown();
+	void run();
+
+	// windows shenanigans
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool frame();
+	void initializeWindows(int&, int&);
+	void shutdownWindows();
+
+	LPCWSTR m_applicationName;
+	HINSTANCE m_instance;
+	HWND m_hwmd;
+
+	/*
+	InputClass* m_input;
+	ApplicationClass* m_application;
+	*/
+};
+
+// redirectiong windows messaging into MessageHandler (whatever that means)
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+static SystemClass* ApplicationHandle = 0;
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "SystemClass.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow) {
+	SystemClass* System;
+	bool result;
+
+	System = new SystemClass;
+
+	result = System->initialize();
+
+	if (result) {
+		System->run();
+	}
+
+	System->shutdown();
+	delete System;
+	System = 0;
+	
+	return 0;
+}
